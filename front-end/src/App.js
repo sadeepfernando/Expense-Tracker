fd59d5f754a7e54074975e2880a1c9c1b02e5ca9
Fernando005-SSF <sadeepfernando12345@gmail.com>
2024-05-29 10:29:24 +0530

    Created the header part

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import Graph from './components/Graph';
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Expense Tracker</h1>
+      <div className="container mx-auto max-w-7xl text-center drop-shadow-lg text-grey-800">
+      <h1 className="text-4xl py-8 mb-10 bg-slate-800 text-white rounded">Expense Tracker</h1>
+
+      {/* Grid colunms */}
+      <div className="grid md:grid-cols-2 gap-4">
+        {/* chart */}
+        <Graph></Graph>
+        {/* form */}
+      </div>
+      </div>
     </div>
   );
 }
