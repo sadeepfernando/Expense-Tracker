c7c9b0c026f468fe0f9e09a94eaaa227a5734c58
Fernando005-SSF <sadeepfernando12345@gmail.com>
2024-05-29 13:13:14 +0530

    created the doughnut chart

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import Graph from './components/Graph';
+import Graph from './Components/Graph';
 
 
 
 function App() {
   return (
     <div className="App">
-      <div className="container mx-auto max-w-7xl text-center drop-shadow-lg text-grey-800">
+      <div className="container mx-auto max-w-6xl text-center drop-shadow-lg text-grey-800">
       <h1 className="text-4xl py-8 mb-10 bg-slate-800 text-white rounded">Expense Tracker</h1>
 
       {/* Grid colunms */}
--- front-end/src/components/Graph.js
+++ front-end/src/components/Graph.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from 'react';
+import { Doughnut } from 'react-chartjs-2';
+import { Chart, ArcElement } from 'chart.js';
+
+Chart.register(ArcElement);
+
+const data = {
+ 
+  
+};
+
+const config = {
+  data : {
+    datasets: [{
+      label: 'My First Dataset',
+      data: [300, 50, 100],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)'
+      ],
+      hoverOffset: 4,
+      borderRadius : 20,
+      spacing : 5
+    }]
+  },
+  options :{
+    cutout :115
+  }
+}
 
 const Graph = () => {
   return (
-    <div>
-      Graph
+    <div className='flex jutify-content max-w-xs mx-auto'>
+      <div className='item'>
+        <div className='chart relative'>
+
+          <Doughnut {...config}></Doughnut>
+          <h3 className='mb-4 font-bold title'>Total
+            <span className='block text-3xl text-emerald-400'>${0}</span></h3>
+            
+        </div>
+        <div className='flex flex-col py-10 gap-4'>
+          {/* labels */}
+        </div>
+      </div>
+      
     </div>
   )
 }
