624da22ad6ab0b9e1aa2d2c32bbb97152a10dd99
Fernando005-SSF <sadeepfernando12345@gmail.com>
2024-06-01 18:18:19 +0530

    Created to itterate over the transaction list

--- front-end/src/components/List.js
+++ front-end/src/components/List.js
@@ -1,9 +1,28 @@
 import React from 'react'
 
+
+const obj =[
+  {
+      name:'Savings',
+      color:'rgb(255, 99, 132)',
+  },
+  {
+     name:'Investements',
+      color:'rgb(255, 99, 132)',
+  },
+  {
+      name:'Expenses',
+      color:'rgb(255, 99, 132)',
+  },
+]
+
+
 export default function List() {
   return (
     <div className='flex flex-col py-6 gap-3'>
         <h1 className='py-4 font-bold text-2xl'>Transaction History</h1>
+
+      { obj.map((value, key) =><Transaction key={key} category={(value)}></Transaction>) }
       
     </div>
   )
