e4baa0f7eace6d6a2eb87cd536d5332ea82e6492
Fernando005-SSF <sadeepfernando12345@gmail.com>
2024-06-05 10:02:35 +0530

    redux toolkit query

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { store } from './store/store';
 import { provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <provider>
+  <provider store = {store}>
     <App />
   </provider>
 );
