5c16cec83fa3fffb95e88507f56a3be2591be27a
Fernando005-SSF <sadeepfernando12345@gmail.com>
2024-06-07 20:31:34 +0530

    Get code execution

--- front-end/src/store/reducer.js
+++ front-end/src/store/reducer.js
@@ -11,7 +11,7 @@ export const expenseSlice = createSlice({
     initialState,
     reducers: {
         getTransactions: (state) =>{
-            //get code
+            //Todo - get code
         }
     }
 })
