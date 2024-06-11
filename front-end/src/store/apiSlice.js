e6430824e0db4a2fa140c3a7c47683520298f04c
Fernando005-SSF <sadeepfernando12345@gmail.com>
2024-06-11 21:02:28 +0530

    fetch data from backend label section

--- front-end/src/store/apiSlice.js
+++ front-end/src/store/apiSlice.js
@@ -5,8 +5,15 @@ const baseURL = 'http://localhost:5000';
 export const apiSlice = createApi({
     baseQuery : fetchBaseQuery({baseUrl:baseURL}),
     endpoints: builder => ({
+
+        //Get categories
         getCategories : builder.query({
             query:() => '/api/categories'
+        }),
+
+        //Get labels
+        getLabels : builder.query({
+            query : () => '/api/labels'
         })
     })
 })
