febc70f4c5f3fd2ee8b6cfa665584ab365beba24
Fernando005-SSF <sadeepfernando12345@gmail.com>
2024-06-12 14:21:11 +0530

    created to delete the transactions from the delete icon

--- front-end/src/components/List.js
+++ front-end/src/components/List.js
@@ -7,13 +7,23 @@ import { default as api } from '../store/apiSlice';
 export default function List() {
 
   const{ data, isFetching, isError, isSuccess } = api.useGetLabelsQuery();
+  const [deleteTransaction] = api.useDeleteTransactionMutation();
+
+
+
+    const handleClick = async(e) =>{
+
+      if(!e.target.dataset.id) return 0;
+
+      await deleteTransaction({_id : e.target.dataset.id });
+    }
     
     let Transactions;
 
     if(isFetching){
         Transactions = <div>Fetching</div>
     }else if(isSuccess){
-        Transactions =  data.map((value, key) =><Transaction key={key} category={(value)}></Transaction>) 
+        Transactions =  data.map((value, key) =><Transaction key={key} category={(value)} handler ={handleClick}></Transaction>) 
         
     }else if(isError){
         Transactions = <div>Error</div>
@@ -29,12 +39,12 @@ export default function List() {
   )
 }
 
-function Transaction({category}){
+function Transaction({category , handler}){
     if(!category) return null;
 
     return(
         <div className="item flex justify-center bg-gray-50 py-2 rounded-r" style={{borderRight: `8px solid ${category.color ?? '#e5e5e5' }`}}>
-          <button className='px-3'><box-icon name='trash' size='15px'></box-icon></button>
+          <button className='px-3' onClick={ handler }><box-icon data-id = {category._id ?? ''} name='trash' size='15px'></box-icon></button>
             <span className='block w-full'>{category.name ?? ''}</span>
         </div>
     )
